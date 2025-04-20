e8677c97ebfe81689e8c9dbfae7f0eba58c7bef9
kelvin0307 <82506575+kelvin0307@users.noreply.github.com>
2025-04-20 22:53:47 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module KnightsQuest {
+    requires hanyaeger;
+
+    exports org.example;
+
+    //als je subfolders gaat gebruiken in resources.
+    //bijv. subfolder tiles
+    //Zet hier dan
+    // opens tiles;
+
+}
---
+++ src/main/java/org/example/KnightsQuest.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import com.github.hanyaeger.api.YaegerGame;
+
+/**
+ * Hello world!
+ *
+ */
+public class KnightsQuest extends YaegerGame
+{
+    public static void main( String[] args )
+    {
+        launch(args);
+    }
+
+    @Override
+    public void setupGame() {
+
+    }
+
+    @Override
+    public void setupScenes() {
+
+    }
+}
