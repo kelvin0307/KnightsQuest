0c5ee08621576392f50fa9eed6a94071d0b07e7e
bsiebers <bwsiebers@gmail.com>
2025-04-30 19:10:40 +0200

    losse pngs

--- src/main/java/org/example/FirstGameMap.java
+++ src/main/java/org/example/FirstGameMap.java
@@ -2,6 +2,7 @@ package org.example;
 
 import com.github.hanyaeger.api.scenes.TileMap;
 
+
 @SuppressWarnings("serial")
 public class FirstGameMap extends TileMap {
 
--- src/main/java/org/example/entities/tiles/Tile.java
+++ src/main/java/org/example/entities/tiles/Tile.java
@@ -1,14 +1,5 @@
 package org.example.entities.tiles;
 
-import com.github.hanyaeger.api.Coordinate2D;
-import com.github.hanyaeger.api.Size;
-import com.github.hanyaeger.api.entities.impl.SpriteEntity;
-
-public abstract class Tile extends SpriteEntity {
-
-	protected Tile(String resource, Coordinate2D initialLocation, Size size) {
-		super(resource, initialLocation, size);
-		
-	}
+public class Tile {
 
 }
