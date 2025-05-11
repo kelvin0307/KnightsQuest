84a305b115a72657c252a0c34e1481f34b46101f
bsiebers <bwsiebers@gmail.com>
2025-05-11 19:00:17 +0200

    level Select

--- src/main/java/org/example/SelectLevelScene.java
+++ src/main/java/org/example/SelectLevelScene.java
@@ -1,8 +1,10 @@
 package org.example;
 
 import org.example.entities.buttons.FirstLevelButton;
+import org.example.entities.text.Level1Text;
 
 import com.github.hanyaeger.api.Coordinate2D;
+import com.github.hanyaeger.api.Size;
 import com.github.hanyaeger.api.scenes.StaticScene;
 
 public class SelectLevelScene extends StaticScene {
@@ -22,7 +24,8 @@ public class SelectLevelScene extends StaticScene {
 		var level1Button = new FirstLevelButton(new Coordinate2D(200, 200), knightsQuest);
 		addEntity(level1Button);
 		
-		
+		var level1Text = new Level1Text(new Coordinate2D(250, 125), new Size(64, 64));
+		addEntity(level1Text);
 	}
 
 }
